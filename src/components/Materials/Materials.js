5cdbfed22f40d26a810763abd3091a27369efbed
Terence Mangram <tmangram@gmail.com>
2018-03-04 16:08:02 +0900

    Add base code

---
+++ src/components/Materials/Materials.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import styles from "./Materials.css";
+
+const materials = (props) => {
+
+	return (
+		
+	);
+};
+
+export default materials;
--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -1,12 +1,16 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+
+import styles from "./Home.css";
 
 class Home extends Component {
-  render() {
-    return (
-      <div className="Home">
-      </div>
-    );
-  }
+	state = {};
+
+	render() {
+		return (
+			<div className="Home">
+			</div>
+		);
+	}
 }
 
 export default Home;
