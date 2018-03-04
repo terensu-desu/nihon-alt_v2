9a73a6c99f319642bdcb30d3bd55869a433b276b
Terence Mangram <tmangram@gmail.com>
2018-03-04 16:07:29 +0900

    Add base router routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
+import Home from "./containers/Home/Home";
+import Materials from "./components/Materials/Materials";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-      </div>
+      <Router className="App">
+        <Route exact path="/" component={Home} />
+        <Route path="/pages/:section/:unit" component={Materials} />
+      </Router>
     );
   }
 }
