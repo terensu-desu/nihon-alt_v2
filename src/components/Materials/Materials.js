589e89f8311e7816918261f62bcbd7f7263fd498
Terence Mangram <tmangram@gmail.com>
2018-03-07 02:53:55 +0900

    Initial commit

--- src/components/Materials/Materials.js
+++ src/components/Materials/Materials.js
@@ -1,11 +1,11 @@
 import React from 'react';
 
-import styles from "./Materials.css";
+//import styles from "./Materials.css";
 
 const materials = (props) => {
 
 	return (
-		
+		<p>Hello</p>
 	);
 };
 
