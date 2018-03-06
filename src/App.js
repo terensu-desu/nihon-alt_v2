1f3dc5d8d75d7c5b382e2f8bb7000d0b6b2c5dc4
Terence Mangram <tmangram@gmail.com>
2018-03-07 02:53:25 +0900

    Add aux component to wrap navbar, banner, and routes

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
 import React, { Component } from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
+import Aux from "./hoc/Aux";
 import Home from "./containers/Home/Home";
+import Banner from "./components/Banner/Banner";
+import Navbar from "./components/Navbar/Navbar";
 import Materials from "./components/Materials/Materials";
 
 class App extends Component {
   render() {
     return (
       <Router className="App">
-        <Route exact path="/" component={Home} />
-        <Route path="/pages/:section/:unit" component={Materials} />
+      	<Aux>
+		    	<Navbar />
+		    	<Banner />
+	      	<Switch>
+		        <Route path="/pages/:section/:unit" component={Materials} />
+		        <Route exact path="/" component={Home} />
+	      	</Switch>
+      	</Aux>
       </Router>
     );
   }
