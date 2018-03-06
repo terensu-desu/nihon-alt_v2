76b945038323252723ae3cfb1954679b27c3dfac
Terence Mangram <tmangram@gmail.com>
2018-03-07 02:55:52 +0900

    Add basic structure layout for blog for testing

---
+++ src/components/Blog/Blog.js
@@ -0,0 +1,35 @@
+import React from 'react';
+
+const blog = (props) => {
+
+	return (
+		<div className="col m12 l8">
+			<div className="card-panel">
+				<h4 className="center">Otsukare News</h4>
+				<ul className="collection">
+					<li className="collection-item">
+			      <h5 className="title">My Transformation from ALT to Super ALT</h5>
+			      <h6 className="red-text">Nate G.</h6>
+			      <p>Lorem ipsum pimpin' sit amizzle, consectetuer adipiscing crunk. Yo sapien velizzle, doggy volutpizzle, suscipizzle crackalackin, gravida vizzle, dizzle. Pellentesque we gonna chung gizzle... <a className="blue-text">Read more</a></p>
+			    </li>
+			    <li className="collection-item">
+			      <h5 className="title">Tips for New ALTs From Grizzled Veterans</h5>
+			      <h6 className="red-text">Matthew K.</h6>
+			      <p>Lorem ipsum pimpin' sit amizzle, consectetuer adipiscing crunk. Yo sapien velizzle, doggy volutpizzle, suscipizzle crackalackin, gravida vizzle, dizzle. Pellentesque we gonna chung gizzle... <a className="blue-text">Read more</a></p>
+			    </li>
+			    <li className="collection-item">
+			      <h5 className="title">Saving Money in Japan - Slash Your Phone Bill</h5>
+			      <h6 className="red-text">Karl G.</h6>
+			      <p>Lorem ipsum pimpin' sit amizzle, consectetuer adipiscing crunk. Yo sapien velizzle, doggy volutpizzle, suscipizzle crackalackin, gravida vizzle, dizzle. Pellentesque we gonna chung gizzle... <a className="blue-text">Read more</a></p>
+			    </li>
+				</ul>
+				<div className="center">
+					<a className="btn">Newer articles <i className="material-icons left">navigate_before</i></a>
+					<a className="btn">Older articles <i className="material-icons right">navigate_next</i></a>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default blog;
