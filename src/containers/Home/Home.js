52cda158733134e931f0ae72759af179cf32cc78
Terence Mangram <tmangram@gmail.com>
2018-03-07 02:54:33 +0900

    Add container classes, import components

--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -1,13 +1,19 @@
 import React, { Component } from "react";
 
-import styles from "./Home.css";
+import Blog from "../../components/Blog/Blog";
+import FileUpload from "../../components/FileUpload/FileUpload";
+//import styles from "./Home.css";
 
 class Home extends Component {
 	state = {};
 
 	render() {
 		return (
-			<div className="Home">
+			<div className="container-fluid">
+				<div className="row">
+					<FileUpload />
+					<Blog />
+				</div>
 			</div>
 		);
 	}
