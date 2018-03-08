83bf6f47a1134e9f62186de666f56fdcd824c1eb
Terence Mangram <tmangram@gmail.com>
2018-03-08 19:24:18 +0900

    Add state to handle form values, initial validation check, onchange handler

--- src/components/FileUpload/FileUpload.js
+++ src/components/FileUpload/FileUpload.js
@@ -2,8 +2,40 @@ import React, { Component } from "react";
 import M from "materialize-css";
 
 class FileUpload extends Component {
+	state = {
+		formData: {
+			name: "",
+			title: "",
+			instructions: "",
+			grade: "",
+			file: ""
+		}
+	}
+
 	componentDidMount() {
 		M.FormSelect.init(this.select)
+	}
+
+	handleOnChange = (event) => {
+		event.preventDefault();
+		this.setState({
+			formData: {
+				[event.target.name]: event.target.value
+			}
+		});
+		this.handleFormValidation();
+	}
+
+	handleFormValidation = () => {
+		let valid = true;
+		for(let key in this.state.formData) {
+			if(this.state.formData[key].length === 0) {
+				valid = false;
+			}
+		}
+		if(valid) {
+			document.getElementById("submitBtn").classList.toggle("disabled");
+		}
 	}
 
 	handleSubmit = (event) => {
@@ -12,6 +44,7 @@ class FileUpload extends Component {
 	}
 
 	render() {
+		console.log(this.state.formData)
 		return (
 			<div className="col m12 l4">
 				<div className="card-panel">
@@ -24,17 +57,21 @@ class FileUpload extends Component {
 									<input 
 										type="text" 
 										className="validate" 
-										id="uploadername" 
-										name="uploadername" 
-										placeholder="Your name" />
+										id="name" 
+										name="name" 
+										placeholder="Your name" 
+										value={this.state.formData.uploaderName}
+										onChange={this.handleOnChange} />
 								</div>
 								<div className="input-field col s6">
 									<input 
 										type="text" 
 										className="validate" 
-										id="filetitle" 
-										name="filetitle" 
-										placeholder="File Title" />
+										id="title" 
+										name="title" 
+										placeholder="File Title" 
+										value={this.state.formData.fileTitle}
+										onChange={this.handleOnChange} />
 								</div>
 							</div>
 							<div className="row">
@@ -43,14 +80,17 @@ class FileUpload extends Component {
 										name="instructions" 
 										id="instructions"
 										placeholder="Enter detailed instructions here."
-										className="materialize-textarea">
+										className="materialize-textarea"
+										value={this.state.formData.instructions}
+										onChange={this.handleOnChange}>
 									</textarea>
 								</div>
 							</div>
 							<div className="row">
-								<p className="caption center">What year, class, or club is this mainly for?</p>
+								<p className="caption center">What grade, class, or club is this mainly for?</p>
 								<div className="input-field col s12">
-							    <select ref={(select) => {this.select = select}}>
+							    <select onChange={this.handleOnChange} name="grade" ref={(select) => {this.select = select}}>
+							    	<option value="" disabled selected>Choose one</option>
 						        <option value="elementary">Elementary</option>
 							      <optgroup label="Junior High School">
 							        <option value="jhs-y1">JHS Year 1</option>
@@ -60,19 +100,23 @@ class FileUpload extends Component {
 						        <option value="specialneeds">Special Needs</option>
 						        <option value="highschool">High School</option>
 							    </select>
-							    <label>Optgroups</label>
+							    <label>This is for:</label>
 							  </div>
 							</div>
 							<div className="row">
 								<div className="file-field input-field">
-									<div className="btn">File <input type="file" /></div>
+									<div onChange={this.handleOnChange} className="btn">File <input name="file" type="file" /></div>
 									<div className="file-path-wrapper">
-										<input type="text" className="file-path validate"/>
+										<input type="text" name="filepath" className="file-path validate"/>
 									</div>
 								</div>
 							</div>
 							<div className="center">
-								<button className="btn">Submit <i className="material-icons right">send</i></button>
+								<button 
+									id="submitBtn" 
+									className="btn disabled">
+										Submit <i className="material-icons right">send</i>
+								</button>
 							</div>
 						</form>
 					</div>
