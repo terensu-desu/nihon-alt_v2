b628348d5035b8a5645d8becbf2ea43e0a8ec755
Terence Mangram <tmangram@gmail.com>
2018-04-29 08:20:54 -0500

    Remove redux tools and related imports

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from "react-redux";
-import { createStore, combineReducers, applyMiddleware, compose } from "redux";
+import { createStore, combineReducers, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
 
 import authReducer from "./store/reducers/auth";
